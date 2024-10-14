d4fb38e6a447fb7ccd7051611ed979f17a12f4e0
Kartik <kartikarora23082003@gmail.com>
2024-10-14 14:53:52 +0530

    changed size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,30 +11,30 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <h1 className="justify-center items-center text-center py-2 font-bold text-[28px]">Categories</h1>
       <div className="flex">
         <Link
           to="/product/men's clothing"
-          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-blue-500 to-blue-900 text-white h-72 w-52 m-12 text-[24px] rounded hover:scale-105 duration-75 overflow-hidden"
+          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-blue-500 to-blue-900 text-white h-[450px] w-80 m-12 text-[24px] rounded hover:scale-105 duration-75 overflow-hidden"
         >
           <h1>Men's Clothing</h1>
         </Link>
         <Link
           to="/product/women's clothing"
-          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-pink-900 to-pink-500 text-white h-72 w-52 m-12 text-[20px] rounded hover:scale-105 duration-75 overflow-hidden"
+          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-pink-900 to-pink-500 text-white h-[450px] w-80 m-12 text-[20px] rounded hover:scale-105 duration-75 overflow-hidden"
         >
           <h1>Women's Clothing</h1>
         </Link>
         <Link
           to="/product/jewelery"
-          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-yellow-500 to-yellow-900 text-white h-72 w-52 m-12 text-[24px] rounded hover:scale-105 duration-75 overflow-hidden"
+          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-yellow-500 to-yellow-900 text-white h-[450px] w-80 m-12 text-[24px] rounded hover:scale-105 duration-75 overflow-hidden"
         >
           <h1>Jewelery</h1>
         </Link>
         <Link
           to="/product/electronics"
-          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-red-900 to-red-500 text-white h-72 w-52 m-12 text-[24px] rounded hover:scale-105 duration-75 overflow-hidden"
+          className="flex items-center justify-center font-bold font-serif bg-gradient-to-b from-red-900 to-red-500 text-white h-[450px] w-80 m-12 text-[24px] rounded hover:scale-105 duration-75 overflow-hidden"
         >
           <h1>Electronics</h1>
         </Link>
